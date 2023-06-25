a73dbfd5557ffd850f5d9aad791f35ce322d0a6d
avimi13 <alfredorequenez57libra@gmail.com>
2023-06-25 17:27:12 -0600

    Changes in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { DatabaseModule } from './modules/database/database.module';
-import { SecurityModule } from './modules/security/security.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, SecurityModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/security/security.module.ts
+++ src/modules/security/security.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './controller/users.module';
+
 import { UsersService } from './service/users.service';
 
 @Module({
-  imports: [UsersModule],
-  providers: [UsersService]
+  imports: [],
+  providers: [UsersService],
 })
 export class SecurityModule {}
